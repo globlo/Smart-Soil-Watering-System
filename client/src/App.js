4cc621a76bd4e053430d1caa2a5214e696940a33
globlo <49570577+globlo@users.noreply.github.com>
2023-04-10 13:21:15 -0500

    connect react and node.js
    (not db data) but dummy data

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,31 @@
-
 import './App.css';
+import React, { useEffect, useState} from 'react'
 
 function App() {
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/getMoistures").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [])
+
   return (
     <div className="App">
-      Hey Now!
+
+      {(typeof backendData.users === 'undefined') ? (
+        <p>Loading</p>
+      ) : backendData.users.map((user, i) => (
+        <p key={i}>{user}</p>
+      ))}
+      
     </div>
+
+    // <div>heynow</div>
   );
 }
 
