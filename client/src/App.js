e1d255005cabf37c017e660ac50e69af9a2b818e
globlo <49570577+globlo@users.noreply.github.com>
2023-04-21 22:09:18 -0500

    combine with UI, have not apply the data in the correct format

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
 import React, { useState, useEffect } from "react";
+import RealTimeData from './components/RealTimeData';
+import HistoricalData from './components/HistoricalData';
+import Controls from './components/Controls';
 
 function App() {
 
   const [data, setData] = useState(undefined);
-  // ここを修正
+  // 
   useEffect(() => {
     fetch("/getMoisture")
       .then(res => res.json())
@@ -15,12 +18,45 @@ function App() {
    
   console.log(data);
 
+  // Replace these with actual data fetched from your backend.
+  const [soilMoisture, setSoilMoisture] = useState(50);
+  const [pumpStatus, setPumpStatus] = useState(false);
+  const [history, setHistory] = useState([]);
+
+  const togglePump = () => {
+    setPumpStatus(!pumpStatus);
+    // Call your backend API to toggle the pump status.
+  };
+
+  const changeThreshold = (newThreshold) => {
+    // Call your backend API to update the soil moisture threshold.
+  };
+
+  const changeSchedule = (newSchedule) => {
+    // Call your backend API to update the watering schedule.
+  };
+
   return (
     <div className="App">
       
       {data === undefined ? "" : <div>{data.humidity}</div>}
 
+      <header className="App-header">
+        <h1>Soil Watering System</h1>
+      </header>
+      <main>
+        <RealTimeData soilMoisture={soilMoisture} pumpStatus={pumpStatus} />
+        <HistoricalData history={history} />
+        <Controls
+          onTogglePump={togglePump}
+          onChangeThreshold={changeThreshold}
+          onChangeSchedule={changeSchedule}
+        />
+      </main>
+
     </div>
+    
+    
   );
 }
 
